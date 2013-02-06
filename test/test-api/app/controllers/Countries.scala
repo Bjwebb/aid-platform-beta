84062cc4f8e7f108688a90b54118f9f384009d55
Kyle Davidson <k.davidson@kainos.com>
2013-02-06 16:53:14 +0000

    TestAPI - Changing json format for mock data to navigate better - in progress

--- test/test-api/app/controllers/Countries.scala
+++ test/test-api/app/controllers/Countries.scala
@@ -1,49 +1,41 @@
 package controllers
 
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json._
 
 object Countries extends Controller {
 
-val countries = Json.obj(
-      "countries" -> Json.arr(
-        Json.obj(
-          "code" -> "ET",
-          "name" -> "Ethiopia",
-          "budget" -> 238000000
-        ),
-        Json.obj(
-          "code" -> "PK",
-          "name" -> "Pakistan",
-          "budget" -> 267000000
-        ),
-        Json.obj(
-          "code" -> "IN",
-          "name" -> "India",
-          "budget" -> 264000000
-        ),
-        Json.obj(
-          "code" -> "BD",
-          "name" -> "Bangladesh",
-          "budget" -> 210000000
-        ),
-        Json.obj(
-          "code" -> "NG",
-          "name" -> "Nigeria",
-          "budget" -> 185000000
-        )
-      ))
+  val countries: JsValue = Json.parse("""
+    {
+      "country" : [
+      {
+         "code" : "ET",
+         "name" : "Ethiopia",
+         "budget" : 238000000
+      },
+      {
+         "code" : "PK",
+         "name" : "Pakistan",
+         "budget" : 26700000
+      },
+      {
+         "code" : "IN",
+         "name" : "India",
+         "budget" : 264000000
+      }
+    ]
 
-
+   }
+   """)
   def allCountries = Action {
     Ok (
-        countries
+          (countries\"country")
       )
   }
 
   def country(code: String) = Action {
     Ok (
-        code
+         (countries\"country")(0)
       )
   }
 
