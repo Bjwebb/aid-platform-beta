02cafc3d1b66095e0612cab8d324453b2c144b4c
Kyle Davidson <k.davidson@kainos.com>
2013-02-08 10:58:31 +0000

    Test-API: Refactored JSON to allow for direct tree pathing.

--- test/test-api/app/controllers/Countries.scala
+++ test/test-api/app/controllers/Countries.scala
@@ -6,37 +6,49 @@ import play.api.libs.json._
 object Countries extends Controller {
 
   val countries: JsValue = Json.parse("""
-    {
-      "country" : [
-      {
-         "code" : "ET",
-         "name" : "Ethiopia",
-         "budget" : 238000000
-      },
-      {
-         "code" : "PK",
-         "name" : "Pakistan",
-         "budget" : 26700000
-      },
-      {
-         "code" : "IN",
-         "name" : "India",
-         "budget" : 264000000
-      }
-    ]
+                                      {
+                                        "countries" : {
+                                        "ET" : {
+                                                   "name" : "Ethiopia",
+                                                   "summary" : "Ethiopia has experienced impressive growth and development in recent years.",
+                                                   "budget" : 238000000,
+                                                   "active-projects" : 26,
+                                                   "population" : 67760000,
+                                                   "life expectancy" : 59,
+                                                   "income-level" : "low"
+                                                },
+                                        "PK" : {
+                                                   "name" : "Pakistan",
+                                                   "summary" : "Some 60 million people (one in three) live in poverty. Half of all adults, and two out of every three women, are illiterate",
+                                                   "budget" : 26700000,
+                                                   "active-projects" : 12,
+                                                   "population" : 4760200,
+                                                   "life expectancy" : 48,
+                                                   "income-level" : "low"
+                                                },
+                                        "IN" : {
+                                                   "name" : "India",
+                                                   "summary" : "India is a key strategic partner to the UK and the largest democracy in the Commonwealth",
+                                                   "budget" : 264000000,
+                                                   "active-projects" : 4,
+                                                   "population" : 99800000,
+                                                   "life expectancy" : 65,
+                                                   "income-level" :  "mid-low"
+                                               }
+                                      }
 
-   }
-   """)
+                                     }
+                                      """)
   def allCountries = Action {
     Ok (
-          (countries\"country")
-      )
+          (countries \ "countries")
+       )
   }
 
   def country(code: String) = Action {
     Ok (
-         (countries\"country")(0)
-      )
+           (countries \ "countries" \ code)
+       )
   }
 
 
--- test/test-api/app/controllers/Projects.scala
+++ test/test-api/app/controllers/Projects.scala
@@ -1,39 +1,73 @@
 package controllers
 
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json._
 
 object Projects extends Controller {
 
-val projects = Json.obj(
-      "projects" -> Json.arr(
-        Json.obj(
-          "name" -> "Malaria bed nets distributed",
-          "budget" -> 12200000
-        ),
-        Json.obj(
-          "name" -> "Children vaccinated",
-          "budget" -> 12000000
-        ),
-        Json.obj(
-          "name" -> "Access to financial services",
-          "budget" -> 11900000
-        ),
-        Json.obj(
-          "name" -> "Hygiene conditions improved",
-          "budget" -> 7400000
-        ),
-        Json.obj(
-          "name" -> "Emergency food assistance",
-          "budget" -> 6000000
-        )
-      ))
+  val projects: JsValue = Json.parse("""
+                                      {
+                                        "projects" : {
+                                        "GB-1-202990" : {
+                                                   "name" : "Ethiopia: Support for the Ethiopian Health Sector Development Programme",
+                                                   "summary" : "Increased access to and improved quality of health services.",
+                                                   "budget-set" : 275000000,
+                                                   "budget-spent" : 120000000,
+                                                   "start-date" : "1 Dec 2011",
+                                                   "end-date" : "31 Mar 2015",
+                                                   "sector-groups" : [{
+                                                                     "name" : "Research",
+                                                                     "percentage" : "100%"
+                                                                     }]
+                                                },
+                                        "GB-1-113492" : {
 
+                                                    "name" : "Ethiopia: Water, Sanitation and Hygiene Programme",
+                                                    "summary" : "Increase access to affordable and sustainable water supply and sanitation services.",
+                                                    "budget-set" : 66000000,
+                                                    "budget-spent" : 660000000,
+                                                    "start-date" : "4 Feb 2008",
+                                                    "end-date" : "31 Mar 2013",
+                                                    "sector-groups" : [{
+                                                                      "name" : "Livestock",
+                                                                      "percentage" : "90%"
+                                                                      },
+                                                                      {
+                                                                      "name" : "Social Welfare",
+                                                                      "percentage" : "10%"
+                                                                      }]
+                                                    },
+                                        "GB-1-200094" : {
+                                                   "name" : "Africa Enterprise Challenge Fund (AECF)",
+                                                   "summary" : "To catalyse private sector entrepreneurs in Africa to innovate and find profitable ways of improving market access and functioning for the poor- especially in rural areas. Its focus will be on agricultural, targeting agribusiness, and access to finance working with providers of financial services",
+                                                   "budget-set" : 10000000,
+                                                   "budget-spent" : 791534,
+                                                   "start-date" : "17 July 2008",
+                                                   "end-date" : "17 Aug 2018",
+                                                   "sector-groups" : [{
+                                                                          "name" : "Livestock",
+                                                                          "percentage" : "90%"
+                                                                       },
+                                                                       {
+                                                                          "name" : "Social Welfare",
+                                                                          "percentage" : "10%"
+                                                                       }]
+                                               }
+                                      }
+
+                                     }
+                                     """)
 
   def allProjects = Action {
     Ok (
-        projects
+        (projects \ "projects")
       )
+  }
+
+  def project(code: String) = Action {
+    Ok (
+        (projects \ "projects" \ code)
+    )
   }
 
 
