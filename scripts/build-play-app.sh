d3ab5785b1383f9eb1f267c6355ac7921f783945
James Hughes <james@yobriefca.se>
2013-02-15 14:16:24 +0000

    Addition of build scripts for all components

---
+++ scripts/build-play-app.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update current repo
+git pull origin master
+
+# Update the Build Submodule
+cd ./build
+git pull origin master
+cd ..
+
+# Remove curent aretfacts
+rm -rf "./build/artefacts/"$1
+mkdir -p "./build/artefacts/"$1
+
+# Build new artefacts
+cd "./src/"$1
+play clean dist
+cd ../..
+
+# Move new artefacts
+mv "./src/"$1"/dist/"*.zip "./build/artefacts/"$1
+
+# Commit new artefacts
+cd build
+git add -A
+git commit -m "Automated Commit: "$1
+git push origin master
+cd ..
---
+++ scripts/build-search.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./scripts/build-play-app.sh search
