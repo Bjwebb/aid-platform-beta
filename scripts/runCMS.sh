a3a3c5091d7e862ee6adbac8c4ece1b41b64f35b
root <root@ip-10-63-11-126.eu-west-1.compute.internal>
2013-02-21 10:05:23 +0000

    Add api launch to CI

--- scripts/runCMS.sh
+++ scripts/runCMS.sh
@@ -10,15 +10,15 @@ echo "Deploying and running cms application..."
 unzip -o ./build/artefacts/cms/*.zip -d ./running/
 chmod a+x ./running/cms*/start
 ./scripts/playctrl.sh running/cms-1.0-SNAPSHOT/ start 9000
-#running/cms*/start -Dhttp.port=9000 &
 
 #echo "Deploying and running loader application..."
 #cp ./build/artefacts/loader/*-dist.jar ./running/
 #java -jar ./running/loader*-dist.jar
 
-#echo "Deploying and running cms application..."
-#unzip -o ./build/artefacts/api/*.zip -d ./running/
-#chmod a+x ./running/api*/start
+echo "Deploying and running api application..."
+unzip -o ./build/artefacts/api/*.zip -d ./running/
+chmod a+x ./running/api*/start
+./scripts/playctrl.sh running/api-1.0-SNAPSHOT/ start 9001
 #./running/api*/start -Dhttp.port=9001 &
 
 #echo "Deploying and running search application..."
